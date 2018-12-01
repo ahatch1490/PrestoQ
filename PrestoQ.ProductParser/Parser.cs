726a930a4a6f0f29638a6549e38ccb6a9919a324
ahatch1490 <ahatch1490@gmail.com>
2018-12-01 08:39:13 -0800

    moving proeprty setters to new class to de-clutter the Parser class

--- PrestoQ.ProductParser/Parser.cs
+++ PrestoQ.ProductParser/Parser.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
-using System.Threading;
+using static PrestoQ.ProductParser.PropertyParser;
 
 namespace PrestoQ.ProductParser
 {
-    public class Parser
+    public static class Parser
     {
-        private const decimal TAX = 0.7775m; 
-        private const int WEIGHT_INDEX = 2; 
-        private const int TAXABLE_INDEX = 4; 
         
         public static List<Product> GetProducts(string data)
         {
@@ -37,17 +31,7 @@ namespace PrestoQ.ProductParser
             }
             return products;
         }
-
-        private static bool IsLastProduct(string line)
-        {
-            return line == null;
-        }
-
-        private static bool ValidProductLine(string line)
-        {
-            return ! string.IsNullOrWhiteSpace(line); 
-        }
-
+        
         private static Product CreateProduct(string line)
         {
             return new Product
@@ -64,100 +48,19 @@ namespace PrestoQ.ProductParser
             }; 
         }
 
-        private static string GetDescription(string line)
+        private static bool IsLastProduct(string line)
         {
-            return line.Substring(9,59).Trim();
+            return line == null;
         }
 
-        private static int GetProductID(string line)
+        private static bool ValidProductLine(string line)
         {
-            var id = FormatNumber(0, 8, line); 
-            return Convert.ToInt32(id); 
+            return ! string.IsNullOrWhiteSpace(line); 
         }
 
-        private static decimal GetRegularSingularPrice(string line)
-        {
-            var regPrice = FormatNumber(69, 8, line); 
-            return string.IsNullOrWhiteSpace(regPrice) ? 0.00m : FormatPrice(regPrice);
-        }
+      
+
         
-        private static decimal FormatPrice(string value)
-        {
-            var lastIndex = value.Length - 2;
-            var price = value.Insert(lastIndex, ".");
-            return decimal.Parse(price); 
-        }
-        
-        private static decimal PromotionalSingularPrice(string line)
-        {
-            var promoPrice = FormatNumber(78, 8, line); 
-            return string.IsNullOrWhiteSpace(promoPrice) ? 0.00m : FormatPrice(promoPrice);        
-        }
-        
-        private static decimal GetRegularSplitPrice(string line)
-        {
-            var regSplitPrice = FormatNumber(87, 8, line);
-            var split = FormatNumber(105, 8, line);
-
-            if (string.IsNullOrWhiteSpace(regSplitPrice) || string.IsNullOrWhiteSpace(regSplitPrice))
-            {
-                return 0.00m; 
-            }
-            return FormatSplitPrice(FormatPrice(regSplitPrice),decimal.Parse(split));  
-        }
-        
-        private static decimal GetPromotionalSplitPrice(string line)
-        {
-            var promoSplitPrice = line.Substring(96,8).TrimStart('0');
-            var split = line.Substring(114, 8).TrimStart('0');
-
-            if (string.IsNullOrWhiteSpace(promoSplitPrice) || string.IsNullOrWhiteSpace(promoSplitPrice))
-            {
-                return 0.00m; 
-            }
-            return FormatSplitPrice(FormatPrice(promoSplitPrice),decimal.Parse(split));  
-        }
-
-        private static decimal GetTaxRate(string line)
-        {
-            return GetFlag(line, TAXABLE_INDEX) ? TAX : 0.00m;
-        }
-        
-        private static string GetUnitOfMeasure(string line)
-        {
-            var measure = "Each";
-            if (GetFlag(line,WEIGHT_INDEX))
-            {
-                measure = "Pound"; 
-            }
-            return measure; 
-        }
-
-        private static  bool GetFlag(string line, int flagIndex)
-        {
-            var flags = line.Substring(123, 8);
-            if (string.IsNullOrWhiteSpace(flags))
-            {
-                return false;
-            }
-
-            return flags[flagIndex] == 'Y';
-        }
-
-        private static decimal FormatSplitPrice(decimal regSplitPrice, decimal split)
-        {
-            return Math.Round(regSplitPrice / split, 4);
-        }
-        
-        private static string GetProductSize(string line)
-        {
-            return line.Substring(134, 8).Trim();
-        }
-
-        private static string FormatNumber(int start, int length, string line)
-        {
-            return line.Substring(start, length).TrimStart('0');
-        }
  
     }
 }
---
+++ PrestoQ.ProductParser/PropertyParser.cs
@@ -0,0 +1,106 @@
+using System;
+
+namespace PrestoQ.ProductParser
+{
+    internal static class PropertyParser
+    {
+        private const decimal TAX = 0.7775m; 
+        private const int WEIGHT_INDEX = 2; 
+        private const int TAXABLE_INDEX = 4; 
+        
+        public static string GetDescription(string line)
+        {
+            return line.Substring(9,59).Trim();
+        }
+
+        public static int GetProductID(string line)
+        {
+            var id = FormatNumber(0, 8, line); 
+            return Convert.ToInt32(id); 
+        }
+
+        public static decimal GetRegularSingularPrice(string line)
+        {
+            var regPrice = FormatNumber(69, 8, line); 
+            return string.IsNullOrWhiteSpace(regPrice) ? 0.00m : FormatPrice(regPrice);
+        }
+        
+        public static decimal PromotionalSingularPrice(string line)
+        {
+            var promoPrice = FormatNumber(78, 8, line); 
+            return string.IsNullOrWhiteSpace(promoPrice) ? 0.00m : FormatPrice(promoPrice);        
+        }
+        
+        public static decimal GetRegularSplitPrice(string line)
+        {
+            var regSplitPrice = FormatNumber(87, 8, line);
+            var split = FormatNumber(105, 8, line);
+
+            if (string.IsNullOrWhiteSpace(regSplitPrice) || string.IsNullOrWhiteSpace(regSplitPrice))
+            {
+                return 0.00m; 
+            }
+            return SplitPrice(FormatPrice(regSplitPrice),decimal.Parse(split));  
+        }
+        
+        public static decimal GetPromotionalSplitPrice(string line)
+        {
+            var promoSplitPrice = line.Substring(96,8).TrimStart('0');
+            var split = line.Substring(114, 8).TrimStart('0');
+
+            if (string.IsNullOrWhiteSpace(promoSplitPrice) || string.IsNullOrWhiteSpace(promoSplitPrice))
+            {
+                return 0.00m; 
+            }
+            return SplitPrice(FormatPrice(promoSplitPrice),decimal.Parse(split));  
+        }
+
+        public static decimal GetTaxRate(string line)
+        {
+            return GetFlag(line, TAXABLE_INDEX) ? TAX : 0.00m;
+        }
+        
+        public static string GetUnitOfMeasure(string line)
+        {
+            var measure = "Each";
+            if (GetFlag(line,WEIGHT_INDEX))
+            {
+                measure = "Pound"; 
+            }
+            return measure; 
+        }
+
+        private static  bool GetFlag(string line, int flagIndex)
+        {
+            var flags = line.Substring(123, 8);
+            if (string.IsNullOrWhiteSpace(flags))
+            {
+                return false;
+            }
+
+            return flags[flagIndex] == 'Y';
+        }
+        
+        private static decimal FormatPrice(string value)
+        {
+            var lastIndex = value.Length - 2;
+            var price = value.Insert(lastIndex, ".");
+            return decimal.Parse(price); 
+        }
+
+        private static decimal SplitPrice(decimal splitPrice, decimal split)
+        {
+            return Math.Round(splitPrice / split, 4);
+        }
+
+        public static string GetProductSize(string line)
+        {
+            return line.Substring(134, 8).Trim();
+        }
+
+        private static string FormatNumber(int start, int length, string line)
+        {
+            return line.Substring(start, length).TrimStart('0');
+        }
+    }
+}
